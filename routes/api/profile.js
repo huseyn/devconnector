c775e66d19dcc3a5eb34a4f9be9458c02b5c4808
Huseyn Mikayil <huseyn-mikayil@mail.ru>
2020-07-20 17:30:03 +0400

    Create - get all profiles and get profile by user id api

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -24,7 +24,7 @@ router.get('/me', auth, async (req, res) => {
     }
 });
 
-// @route   POST api/profile/me
+// @route   POST api/profile/
 // @desc    Create or update user profile
 // @access  Private
 router.post('/', [auth, [
@@ -100,6 +100,42 @@ router.post('/', [auth, [
         res.status(500).send('Server Error');
     }
 
-})
+});
+
+// @route   GET api/profile/
+// @desc    Get all profiles
+// @access  Public
+router.get('/', async (req, res) => {
+    try {
+        const profiles = await Profile.find().populate('user', ['name', 'avatar']);
+        res.json(profiles);
+    } catch (err) {
+        console.error(err.message);
+        res.status(500).send('Server Error');
+    }
+});
+
+// @route   GET api/profile/user/:user_id
+// @desc    Get profile by user ID
+// @access  Public
+router.get('/user/:user_id', async (req, res) => {
+    try {
+        const profile = await Profile
+            .findOne({ user: req.params.user_id })
+            .populate('user', ['name', 'avatar']);
+
+        if (!profile)
+            return res.status(400).json({ msg: 'Profile not found' });
+
+        res.json(profile);
+    } catch (err) {
+        console.error(err.message);
+        if (err.kind == 'ObjectId') {
+            return res.status(400).json({ msg: 'Profile not found' });
+        }
+        res.status(500).send('Server Error');
+    }
+});
+
 
 module.exports = router;
