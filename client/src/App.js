893636f3c7c02c2e0a50b1a57258eb7dd0bc8cfe
Huseyn Mikayil <huseyn-mikayil@mail.ru>
2020-07-27 22:44:12 +0400

    Create - add experience and education functionality

--- client/src/App.js
+++ client/src/App.js
@@ -8,6 +8,8 @@ import Alert from './components/layout/Alert';
 import Dashboard from './components/dashboard/Dashboard';
 import CreateProfile from './components/profile-form/CreateProfile';
 import EditProfile from './components/profile-form/EditProfile';
+import AddExperience from './components/profile-form/AddExperience';
+import AddEducation from './components/profile-form/AddEducation';
 import PrivateRoute from './components/routing/PrivateRoute';
 
 import store from './redux/store';
@@ -16,6 +18,7 @@ import { loadUser } from './redux/actions/auth';
 import setAuthToken from './utils/setAuthToken';
 
 import './App.css';
+import { addEducation } from './redux/actions/profile';
 
 
 if (localStorage.token) {
@@ -41,6 +44,8 @@ const App = () => {
             <PrivateRoute exact path='/dashboard' component={Dashboard} />
             <PrivateRoute exact path='/create-profile' component={CreateProfile} />
             <PrivateRoute exact path='/edit-profile' component={EditProfile} />
+            <PrivateRoute exact path='/add-experience' component={AddExperience} />
+            <PrivateRoute exact path='/add-education' component={AddEducation} />
           </Switch>
         </section>
       </Fragment>
--- client/src/redux/reducers/profile.js
+++ client/src/redux/reducers/profile.js
@@ -1,7 +1,8 @@
 import {
     GET_PROFILE,
     CLEAR_PROFILE,
-    PROFILE_ERROR
+    PROFILE_ERROR,
+    UPDATE_PROFILE
 } from '../actions/types';
 
 const initialState = {
@@ -17,6 +18,7 @@ export default function (state = initialState, action) {
 
     switch (type) {
         case GET_PROFILE:
+        case UPDATE_PROFILE:
             return {
                 ...state,
                 profile: payload,
