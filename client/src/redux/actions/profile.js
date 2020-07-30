ea71bbc1a9375e8dc1cda3e33878add1c3717ca2
Huseyn <huseyn.mikayil95@gmail.com>
2020-07-30 17:39:55 +0400

    Display profile experience and education

--- client/src/redux/actions/profile.js
+++ client/src/redux/actions/profile.js
@@ -30,6 +30,7 @@ export const getCurrentProfile = () => async dispatch => {
 
 // Get all profiles
 export const getProfiles = () => async dispatch => {
+    dispatch({ type: CLEAR_PROFILE });
     try {
         const res = await axios.get('/api/profile/');
 
--- client/src/redux/reducers/profile.js
+++ client/src/redux/reducers/profile.js
@@ -42,8 +42,7 @@ export default function (state = initialState, action) {
             return {
                 ...state,
                 profile: null,
-                repos: [],
-                loading: false
+                repos: []
             }
         case GET_REPOS:
             return {
