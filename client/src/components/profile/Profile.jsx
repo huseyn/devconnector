6e146083cbad42b421137737296d6c4a02c4f4ad
Huseyn <huseyn.mikayil95@gmail.com>
2020-07-30 14:55:43 +0400

    Create ProfileTop and ProfileAbout components

--- client/src/components/profile/Profile.jsx
+++ client/src/components/profile/Profile.jsx
@@ -2,6 +2,8 @@ import React, { useEffect, Fragment } from "react";
 import PropTypes from "prop-types";
 import { connect } from "react-redux";
 import Spinner from "../layout/Spinner";
+import ProfileTop from './ProfileTop';
+import ProfileAbout from './ProfileAbout';
 import { getProfileById } from "../../redux/actions/profile";
 import { Link } from "react-router-dom";
 
@@ -13,7 +15,7 @@ const Profile = ({
 }) => {
   useEffect(() => {
     getProfileById(match.params.id); // get id from url
-  }, [getProfileById]);
+  }, [getProfileById, match.params.id]);
 
   return (
     <Fragment>
@@ -31,6 +33,10 @@ const Profile = ({
                 Edit Profile
               </Link>
             )}
+          <div className="profile-grid my-1">
+            <ProfileTop profile = {profile} />
+            <ProfileAbout profile = {profile} />
+          </div>
         </Fragment>
       )}
     </Fragment>
