37a3a208e56af5f4a05e6ea472a4f8c653d72452
Huseyn Mikayil <huseyn-mikayil@mail.ru>
2020-07-31 16:13:16 +0400

    Display single post

--- client/src/redux/actions/post.js
+++ client/src/redux/actions/post.js
@@ -1,6 +1,13 @@
 import axios from 'axios';
 import { setAlert } from './alert';
-import { GET_POSTS, POST_ERROR, UPDATE_LIKES, DELETE_POST, ADD_POST } from './types';
+import {
+    GET_POSTS,
+    GET_POST,
+    POST_ERROR,
+    UPDATE_LIKES,
+    DELETE_POST,
+    ADD_POST
+} from './types';
 
 // Get Posts
 export const getPosts = () => async dispatch => {
@@ -92,6 +99,23 @@ export const addPost = (formData) => async dispatch => {
         });
 
         dispatch(setAlert('Post Created', 'success'));
+    } catch (err) {
+        dispatch({
+            type: POST_ERROR,
+            payload: { msg: err.response.statusText, status: err.response.status }
+        })
+    }
+}
+
+// Get Post
+export const getPost = (id) => async dispatch => {
+    try {
+        const res = await axios.get(`/api/posts/${id}`);
+
+        dispatch({
+            type: GET_POST,
+            payload: res.data
+        })
     } catch (err) {
         dispatch({
             type: POST_ERROR,
